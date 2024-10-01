c64a4c33b4385a618b1467c053fafcb9525df575
tahsin <u1704102@student.cuet.ac.bd>
2024-10-02 03:38:06 +0600

    user can log in now

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -5,7 +5,22 @@ const passport = require('passport');
 const router = express.Router();
 
 router.post('/register', authController.register);
-router.post('/login', passport.authenticate('local'), authController.login);
+router.post('/login', (req, res, next) => {
+    passport.authenticate('local', (err, user, info) => {
+        if (err) {
+            return res.status(500).json({ message: 'Internal Server Error' });
+        }
+        if (!user) {
+            return res.status(400).json({ message: info.message || 'Invalid credentials.' });
+        }
+        req.logIn(user, (err) => {
+            if (err) {
+                return res.status(500).json({ message: 'Internal Server Error' });
+            }
+            return res.status(200).json({ message: 'Successfully logged in', user });
+        });
+    })(req, res, next);
+});
 router.post('/logout', authController.logout);
 
 module.exports = router;
--- src/services/passportConfig.js
+++ src/services/passportConfig.js
@@ -6,13 +6,17 @@ const localStrategy = require('passport-local').Strategy;
 const prisma = new PrismaClient();
 
 passport.use(new localStrategy(
+    {
+        usernameField: 'email'
+    },
     async (email, password, done) => {
         try {
             const user = await prisma.user.findUnique( { where: {email} } );
             if(!user) {
                 return done(null, false, { message: 'Invalid credentials!' });
             }
-            const isMatch = await bcrypt.compare(bcrypt.hash(password, 10), user.password);
+            console.log(user.password);
+            const isMatch = await bcrypt.compareSync(password, user.password);
             if (!isMatch) {
                 return done(null, false, { message: 'Invalid credentials.' });
             }
