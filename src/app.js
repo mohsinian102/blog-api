38c4ae0d64f0672805cc832906a60d462ed67549
Tahsin Ahmed Majumder <tahsin.ahmed@welldev.io>
2024-10-01 19:09:41 +0600

    will do the the rest from home

--- src/app.js
+++ src/app.js
@@ -1,7 +1,27 @@
 const express = require('express');
 const {PrismaClient} = require('@prisma/client');
+const authRoutes = require('./routes/authRoutes');
+const bodyParser = require('body-parser');
+const dotenv = require('dotenv').config({ path:'../.env' });
+const session = require('');
+const passport = require('passport');
 
 const prisma = new PrismaClient();
 const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+
+app.use(session({
+    secret : process.env.JWT_SECRET,
+    resave : false,
+    saveUninitialized: false
+
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use('/auth', authRoutes);
 
 module.exports = app;
---
+++ src/routes/authRoutes.js
@@ -0,0 +1,3 @@
+const express = require('express');
+const authController = require('./controllers/authController.js');
+
